8c76f366f17783292f9d5f593b6da3efad4849d0
iturner72 <ian96turner@gmail.com>
2023-01-25 10:29:34 -0700

    spell check mapping <Leader> s

--- lua/iturner/remap.lua
+++ lua/iturner/remap.lua
@@ -14,3 +14,9 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 -- centered search 
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+-- spell check
+vim.keymap.set("n", "<Leader>s", function()
+    vim.o.spell = not vim.o.spell
+    print("spell: " .. tostring(vim.o.spell))
+end)
