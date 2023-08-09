7ef8eaa36334972d95c80ddc63829e8b9facf07e
Ian Turner <ian96turner@gmail.com>
2023-08-09 12:57:30 -0600

    remove lsp ensure installed function

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,12 +2,6 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 
-lsp.ensure_installed({
-	'tsserver',
-	'eslint',
-	'rust_analyzer',
-})
-
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
