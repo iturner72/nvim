9a144cdc758302720fe8db545195ccb7b91da84b
Ian Turner <eze@Ians-MacBook-Pro.local>
2023-11-17 10:53:02 -0700

    update packer.lua for latest lsp-zero version

--- lua/iturner/packer.lua
+++ lua/iturner/packer.lua
@@ -8,7 +8,7 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
@@ -35,6 +35,7 @@ return require('packer').startup(function(use)
 
   use {
 	  'VonHeikemen/lsp-zero.nvim',
+      branch = 'v1.x',
 	  requires = {
 		  -- LSP Support
 		  {'neovim/nvim-lspconfig'},
