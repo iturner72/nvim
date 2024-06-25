002b018f34981b69fb2a5f20d5bf854a131db568
iturner72 <ian96turner@gmail.com>
2024-06-24 18:54:07 -0700

    quickfixlist arch btw

--- lua/iturner/set.lua
+++ lua/iturner/set.lua
@@ -31,13 +31,15 @@ vim.g.mapleader = " "
 
 local is_mac = vim.fn.has('mac') == 1
 local is_wsl = vim.fn.has('wsl') == 1
+local is_arch = vim.fn.has('unix') == 1 and io.popen('uname -a'):read('*a'):match('arch')
 
 if is_mac then
     vim.g.vimtex_view_method = 'skim'
     vim.g.maplocalleader = ","
-elseif is_wsl then
+elseif is_wsl or is_arch then
     vim.g.vimtex_view_method = 'zathura'
     vim.g.maplocalleader = ","
+    vim.g.vimtex_quickfix_mode = 0
 end
 
 -- clipboard
