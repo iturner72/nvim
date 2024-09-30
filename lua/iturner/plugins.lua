686c0e4c6203c91ea9dac7b97111dd1d3bef44c2
iturner72 <ian96turner@gmail.com>
2024-09-30 00:04:12 -0600

    add oil dot nvim

--- lua/iturner/plugins.lua
+++ lua/iturner/plugins.lua
@@ -71,4 +71,21 @@ return {
       require('iturner.fidget')()
     end,
   },
+  {
+    'stevearc/oil.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    keys = {
+      { '<leader>o',
+      function()
+        require('oil').open()
+      end,
+      desc = '[F]ormat buffer', }
+    },
+    opts = {
+      default_file_explorer = true,
+      view_options = {
+        show_hidden = true,
+      },
+    },
+  },
 }
