c4ab131274f34f89c3be6aa75fe2dd79db5fe0e7
Ian <ian@creovia.io>
2024-10-04 10:57:58 -0700

    add custom path display to lualine

--- lua/iturner/lualine_config.lua
+++ lua/iturner/lualine_config.lua
@@ -1,4 +1,10 @@
 return function()
+  local function filepath()
+    local path = vim.fn.expand("%:~:.:h")
+    if path == "" or path == "." then return "" end
+    return path .. "/"
+  end
+
   require("lualine").setup({
     options = {
       icons_enabled = true,
@@ -21,15 +27,25 @@ return function()
     sections = {
       lualine_a = {'mode'},
       lualine_b = {'branch', 'diff', 'diagnostics'},
-      lualine_c = {'filename'},
-      lualine_x = {'encoding', 'fileformat', 'filetype'},
+      lualine_c = {
+        { filepath, color = { fg= '#8FBCBB', gui = 'italic' } },
+        { 'filename', path = 0 }
+      },
+      lualine_x = {
+        {'encoding', fmt = string.upper},
+        {'fileformat', icons_enabled = true},
+        {'filetype', icons_enabled = true}
+      },
       lualine_y = {'progress'},
       lualine_z = {'location'}
     },
     inactive_sections = {
       lualine_a = {},
       lualine_b = {},
-      lualine_c = {'filename'},
+      lualine_c = {
+        { filepath, color = { fg = '#4C566A', gui = 'italic' } },
+        { 'filename', path = 0 }
+      },
       lualine_x = {'location'},
       lualine_y = {},
       lualine_z = {}
