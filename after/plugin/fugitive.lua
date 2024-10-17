46bf0c0b032bfd934cc305a844e5d80dfc3933ef
iturner72 <ian96turner@gmail.com>
2024-10-16 22:49:26 -0600

    add fugitive blame format

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -2,3 +2,5 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 
 vim.keymap.set("n", "gh", "<cmd>diffget //2<CR>")
 vim.keymap.set("n", "gl", "<cmd>diffget //3<CR>")
+
+vim.g.fugitive_blame_format = '%(author_time:%Y-%m-%d %H:%M:%S) %(committer_time:%Y-%m-%d %H:%M:%S) %(author) %(committer) %s'
